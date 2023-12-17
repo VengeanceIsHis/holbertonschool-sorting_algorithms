16e0829b6dd3774b1c9de596a73491c21e3122c2
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-17 12:38:13 -0800

    fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -24,13 +24,14 @@ void swap_n(listint_t **h, listint_t **n1, listint_t *n2)
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *iter, *insert, *tmp;
+
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
 	for (iter = (*list)->next; iter != NULL; iter = tmp)
 	{
 		tmp = iter->next;
 		insert = iter->prev;
-		while (insert != NULL &&iter->n < insert->n)
+		while (insert != NULL && iter->n < insert->n)
 		{
 			swap_n(list, &insert, iter);
 			print_list((const listint_t*)*list);
