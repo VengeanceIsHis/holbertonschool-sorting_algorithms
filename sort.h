e2b9c87c352539614d3627bd64f5b1cc2509405a
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-17 13:00:11 -0800

    Task 2

--- sort.h
+++ sort.h
@@ -29,4 +29,6 @@ void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
+void swap_i(int *a, int *b);
+void selection_sort(int *array, size_t size);
 #endif
