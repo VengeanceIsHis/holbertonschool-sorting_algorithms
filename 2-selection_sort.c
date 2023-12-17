60800ea129f5a753ae1962ad165b9a3e7dbccbda
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-17 13:10:46 -0800

    Task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -5,19 +5,21 @@
  */
 void swap_i(int *a, int *b)
 {
-  int tmp;
-  tmp = *a;
-  *a = *b;
-  *b = tmp;
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 /**
- *
- *
+ *selection_sort - sorts an array using selection method
+ *@array: array of integers
+ *@size: The size of given array
  */
 void selection_sort(int *array, size_t size)
 {
 	int *min;
 	size_t i, j;
+
 	if (array == NULL || size < 2)
 		return;
 	for (i = 0; i < size -1; i++)
