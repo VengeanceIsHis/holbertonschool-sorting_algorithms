dd99e42a6d45e9c195ea3b600fbbe926d6b9b040
VengeanceIsHis <elijahjpoole@gmail.com>
2023-12-20 12:30:24 -0800

    Next test

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,8 @@
 #include "sort.h"
 /**
+ *swap_i - function that swaps integers in an array
+ *@a: first integer to swap
+ *@b: second integer to swap
  */
 void swap_i(int *a, int *b)
 {
@@ -10,12 +13,16 @@ void swap_i(int *a, int *b)
 	*b = tmp;
 }
 /**
- *
+ *partition - function that handles the partition and prints the array
+ *@arr: Array to be viewed
+ *@size: size of array
+ *@low: Beginning of the array
+ *@high:Ending of the array
  */
 size_t partition(int arr[], size_t size, int low, int high)
 {
 	int *pivot, above, below;
-	
+
 	pivot = arr + high;
 	for (above = below = low; below < high; below++)
 	{
@@ -36,10 +43,16 @@ size_t partition(int arr[], size_t size, int low, int high)
 	}
 	return (above);
 }
+/**
+ *helper - function that recursively calls partition
+ *@arr: Array to be used
+ *@size: size of the array
+ *@low: beginning of the array
+ *@high: ending of the array
 void helper(int arr[], size_t size, int low, int high)
 {
 	int part;
-	
+
 	if (high - low > 0)
 	{
 		part = partition(arr, size, low, high);
@@ -47,6 +60,10 @@ void helper(int arr[], size_t size, int low, int high)
 		helper(arr, size, part + 1, high);
 	}
 }
+/**
+*quick_sort - function that handles the quick sort algorithm
+*@arr: array to be used
+*@size: size of the array
 void quick_sort(int arr[], size_t size)
 {
 	if (arr == NULL || size < 2)
